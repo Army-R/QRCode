7c7875ec4fdd569d24e2601252d660f374450df3
Army-R <80074550+Army-R@users.noreply.github.com>
2025-12-08 17:42:03 -0600

    Update and rename QR Encoding file

---
+++ qrEncode.py
@@ -0,0 +1,20 @@
+# QR Code Encoding
+
+# Import necessary libraries
+import qrcode
+
+# Create the QR code
+data = "https://github.com/Army-R" # Data to be encoded
+qr = qrcode.QRCode(
+    version=1,
+    box_size=15,
+    border=7,
+)
+qr.add_data(data)
+qr.make(fit=True)
+
+# Create an image from the QR Code instance
+img = qr.make_image(fill_color="white", back_color="black")
+
+# Save the image to a file
+img.save("qrcode.png")
