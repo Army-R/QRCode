997c67e11a9bcbb071c43b9d5b980bc5f3a28b03
Army-R <80074550+Army-R@users.noreply.github.com>
2025-12-08 17:58:06 -0600

    Update QR encoding colors

--- qrEncode.py
+++ qrEncode.py
@@ -4,9 +4,10 @@
 import qrcode
 
 # Create the QR code
-data = "https://github.com/Army-R" # Data to be encoded
+data =  "https://github.com/Army-R" # Data to be encoded
 qr = qrcode.QRCode(
     version=1,
+    error_correction=qrcode.ERROR_CORRECT_H,
     box_size=15,
     border=7,
 )
@@ -14,7 +15,7 @@ qr.add_data(data)
 qr.make(fit=True)
 
 # Create an image from the QR Code instance
-img = qr.make_image(fill_color="white", back_color="black")
+img = qr.make_image(fill_color="blue", back_color="white")
 
 # Save the image to a file
 img.save("qrcode.png")
